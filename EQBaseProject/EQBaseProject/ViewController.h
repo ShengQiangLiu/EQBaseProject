9e515fb30d2f6de55ccad6380d6c78f5788e1077
ShengQiangLiu <1365688791@qq.com>
2016-02-21 16:17:21 +0800

    New Project

---
+++ EQBaseProject/EQBaseProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EQBaseProject
+//
+//  Created by admin on 16/2/21.
+//  Copyright © 2016年 ShengQiangLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EQBaseProject/EQBaseProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EQBaseProject
+//
+//  Created by admin on 16/2/21.
+//  Copyright © 2016年 ShengQiangLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
